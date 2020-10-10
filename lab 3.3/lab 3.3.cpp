295077981e7a1854587861bf89fe1510203f68ab
Feynom <chreptak12@gmail.com>
2020-10-10 15:50:19 +0300

    lab3.3

---
+++ lab 3.3/lab 3.3.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double x, y, r;
+    
+    cout << "Введіть значення х:";
+    cin >> x;
+    cout << endl;
+    cout << "Введіть значення R:";
+    cin >> r;
+    cout << endl;
+
+    if (x <= -r)
+        y = r;
+    else
+        if (-r <= x && x <= r)
+            y = -sqrt(r * r - x * x) + r;
+        else
+            if (r <= x && x < 6)
+                y = (-3 * x - r * x + 9 * r) / (6 - r);
+            else
+                y = x - 9;
+
+    cout << "Значення у = " << y << endl;
+
+    cin.get();
+    return 0;
+}
